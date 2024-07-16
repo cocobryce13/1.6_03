32da24c77ecdbfd509b6132d1b7ca06ca2cb44fa
sokol <sokol@ASUS>
2024-07-16 22:51:02 +0500

    Добавьте файлы проекта.

---
+++ 1.6_03/1.6_03.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+
+void fibonacci(long number) 
+{
+    long a = 0;
+    long b = 1;
+    long result{};
+
+    std::cout << "Числа Фибоначчи: " << a << " " << b << " ";
+
+    for (long i = 2; i < number; i++) 
+    {
+        result = a + b;
+        a = b;
+        b = result;
+        std::cout << result << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    long number;
+    std::cout << "Введите число: ";
+    std::cin >> number;
+
+    fibonacci(number);
+
+    return 0;
+}
